704935c0d90fc60a24596909fcd1a1834e434be4
JimmFly <>
2022-09-18 18:31:14 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,49 @@
+
+print("Bienvenido a mi trivia sobre la electrónica")
+print("Pondremos aprueba tus conocimientos.")
+print("\n")
+print("1) La corriente eléctrica se mide en?")
+print("\n")
+print("a) Watts")
+print("b) Jouls")
+print("c) Amperios")
+
+respuesta_1 = input("\n Tu respuesta: \n")
+
+#print(respuesta_1)
+
+if respuesta_1 == "c":
+  print("respuesta correcta")
+else:
+  print("\n respuesta incorrecta") 
+print("\n")
+print("2) La resistencia se mide en?")
+print("\n")
+print("a) Watts")
+print("b) Ohmios")
+print("c) Joules")
+
+respuesta_2 = input("\n Tu respuesta: \n")
+
+#print(respuesta_2)
+
+if respuesta_2 == "b":
+  print("respuesta correcta")
+else:
+  print("\n respuesta incorrecta")
+print("\n")
+print("3) La resistencia, ¿Tiene polaridad?")
+print("\n")
+print("a) Verdadero")
+print("b) Falso")
+
+respuesta_3 = input("\n Tu repsuesta: \n")
+
+#print(respuesta_3)
+
+if respuesta_3 == "b":
+  print("respuesta correcta")
+else:
+  print("\n respuesta incorrecta")
+print("\n")
+print("Buen trabajo, gracias por participar")
